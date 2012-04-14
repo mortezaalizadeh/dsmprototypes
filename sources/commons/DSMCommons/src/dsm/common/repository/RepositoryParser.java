5ec54d62661e33a2acb41183592774db71aebcb9
morteza.alizadeh@gmail.com <morteza.alizadeh@gmail.com@40325e92-97ac-80f2-9294-f2279de5e1f8>
2012-04-14 09:04:30 +0000

    git-svn-id: https://dsmprototypes.googlecode.com/svn/trunk@78 40325e92-97ac-80f2-9294-f2279de5e1f8

--- sources/commons/DSMCommons/src/dsm/common/repository/RepositoryParser.java
+++ sources/commons/DSMCommons/src/dsm/common/repository/RepositoryParser.java
@@ -19,4 +19,20 @@ public class RepositoryParser extends DSMManifest {
     public RepositoryParser() {
         super("f239604f-bf65-4d05-9ef1-2a34c8e6411a", "Repository Parser");
     }
+    
+    /**
+     * Loads repository information from an repository file.
+     * 
+     * @param szRepositoryFilePath Repository file path
+     */
+    public void loadRepositoryFromFile(String szRepositoryFilePath) {
+    }
+
+    /**
+     * Loads repository information from passed string content.
+     * 
+     * @param szStringContent String content
+     */
+    public void loadRepositoryFromStringContent(String szStringContent) {
+    }
 }
