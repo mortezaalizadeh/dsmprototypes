810706694c8f6bb9baf7839890e70686b0d4b156
morteza.alizadeh@gmail.com <morteza.alizadeh@gmail.com@40325e92-97ac-80f2-9294-f2279de5e1f8>
2012-05-22 10:26:59 +0000

    updated.
    git-svn-id: https://dsmprototypes.googlecode.com/svn/trunk@107 40325e92-97ac-80f2-9294-f2279de5e1f8

---
+++ sources/tests/DSM/src/dsm/test/DSM.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsm.test;
+
+/**
+ *
+ * @author morteza.alizadeh
+ */
+public class DSM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
