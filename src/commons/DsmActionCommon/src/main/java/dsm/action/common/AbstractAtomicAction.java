e3cc0a36c07a9dee75813cf594f0e659d1a3d553
morteza.alizadeh@gmail.com <morteza.alizadeh@gmail.com@40325e92-97ac-80f2-9294-f2279de5e1f8>
2012-11-03 10:16:50 +0000

    updated.
    git-svn-id: https://dsmprototypes.googlecode.com/svn/trunk@143 40325e92-97ac-80f2-9294-f2279de5e1f8

---
+++ src/commons/DsmActionCommon/src/main/java/dsm/action/common/AbstractAtomicAction.java
@@ -0,0 +1,91 @@
+/**
+ * @author Morteza Alizadeh
+ */
+package dsm.action.common;
+
+import dsm.common.DsmManifest;
+import dsm.common.argument.ArgumentContainer;
+
+/**
+ * AbstractAtomicAction abstract class provides basic operations and attributes to all
+ * atomic action classes.
+ *
+ * This class must be inherited by all atomic action classes in order to use
+ * standardized operations and attributes for them.
+ *
+ * @version 0.1, April 10, 2012
+ * @author Morteza Alizadeh
+ */
+public abstract class AbstractAtomicAction extends DsmManifest implements Action {
+
+    /**
+     * Action name.
+     */
+    private String actionName = "No name specified yet!!!";
+
+    /**
+     * Action type.
+     */
+    private ActionType actionType = ActionType.Atomic;
+
+    /**
+     * AuthorityModality Constructor.
+     *
+     * @param dsmUniqueIdentifier DSM object unique identifier
+     * @param dsmFriendlyName DSM object friendly name
+     * @param newActionName Action name
+     */
+    public AbstractAtomicAction(final String dsmUniqueIdentifier,
+                                final String dsmFriendlyName,
+                                final String newActionName) {
+        super(dsmUniqueIdentifier, dsmFriendlyName);
+
+        this.actionName = newActionName;
+    }
+
+    /**
+     * Returns action name.
+     *
+     * @return Action name
+     */
+    public final String getActionName() {
+        return this.actionName;
+    }
+
+    /**
+     * Sets action name.
+     *
+     * @param newActionName New action name
+     */
+    public final void setActionName(final String newActionName) {
+        this.actionName = newActionName;
+    }
+
+    /**
+     * Returns action type.
+     *
+     * @return Action type
+     */
+    public final ActionType getActionType() {
+        return this.actionType;
+    }
+
+    /**
+     * Sets action type.
+     *
+     * @param newActionType New action type
+     */
+    public final void setActionType(final ActionType newActionType) {
+        this.actionType = newActionType;
+    }
+
+    /**
+     * Executes atomic action.
+     *
+     * This method is the call entry for other modules which intend to execute
+     * the atomic action.
+     *
+     * @param argumentContainer Argument container
+     */
+    public abstract void executeAction(final ArgumentContainer argumentContainer);
+}
---
+++ src/commons/DsmActionCommon/src/main/java/dsm/action/common/AbstractDistributedAction.java
@@ -0,0 +1,91 @@
+/**
+ * @author Morteza Alizadeh
+ */
+package dsm.action.common;
+
+import dsm.common.DsmManifest;
+import dsm.common.argument.ArgumentContainer;
+
+/**
+ * AbstractDistributedAction abstract class provides basic operations and attributes
+ * to all distributed action classes.
+ *
+ * This class must be inherited by all distributed action classes in order to
+ * use standardized operations and attributes for them.
+ *
+ * @version 0.1, April 10, 2012
+ * @author Morteza Alizadeh
+ */
+public abstract class AbstractDistributedAction extends DsmManifest implements Action {
+
+    /**
+     * Action name.
+     */
+    private String actionName = "No name specified yet!!!";
+
+    /**
+     * Action type.
+     */
+    private ActionType actionType = ActionType.Distributed;
+
+    /**
+     * AuthorityModality Constructor.
+     *
+     * @param dsmUniqueIdentifier DSM object unique identifier
+     * @param dsmFriendlyName DSM object friendly name
+     * @param newActionName Action name
+     */
+    public AbstractDistributedAction(final String dsmUniqueIdentifier,
+                                     final String dsmFriendlyName,
+                                     final String newActionName) {
+        super(dsmUniqueIdentifier, dsmFriendlyName);
+
+        this.actionName = newActionName;
+    }
+
+    /**
+     * Returns action name.
+     *
+     * @return Action name
+     */
+    public final String getActionName() {
+        return this.actionName;
+    }
+
+    /**
+     * Sets action name.
+     *
+     * @param newActionName New action name
+     */
+    public final void setActionName(final String newActionName) {
+        this.actionName = newActionName;
+    }
+
+    /**
+     * Returns action type.
+     *
+     * @return Action type
+     */
+    public final ActionType getActionType() {
+        return this.actionType;
+    }
+
+    /**
+     * Sets action type.
+     *
+     * @param newActionType New action type
+     */
+    public final void setActionType(final ActionType newActionType) {
+        this.actionType = newActionType;
+    }
+
+    /**
+     * Executes distributed action.
+     *
+     * This method is the call entry for other modules which intend to execute
+     * the distributed action.
+     *
+     * @param argumentContainer Argument container
+     */
+    public abstract void executeAction(final ArgumentContainer argumentContainer);
+}
