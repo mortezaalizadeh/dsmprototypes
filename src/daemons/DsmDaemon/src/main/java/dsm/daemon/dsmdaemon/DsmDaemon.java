afc2c53fb80a924631c62680dbb7dc4345d9e17f
morteza.alizadeh@gmail.com <morteza.alizadeh@gmail.com@40325e92-97ac-80f2-9294-f2279de5e1f8>
2012-11-05 09:01:33 +0000

    updated.
    git-svn-id: https://dsmprototypes.googlecode.com/svn/trunk@155 40325e92-97ac-80f2-9294-f2279de5e1f8

---
+++ src/daemons/DsmDaemon/src/main/java/dsm/daemon/dsmdaemon/DsmDaemon.java
@@ -0,0 +1,67 @@
+/**
+ * @author Morteza Alizadeh
+ */
+package dsm.daemon.dsmdaemon;
+
+import dsm.common.DsmManifest;
+import dsm.daemon.common.Daemon;
+
+/**
+ * DsmDaemon is a test daemon bringing up test modules during tests.
+ *
+ * @version 0.1, April 14, 2012
+ * @author Morteza Alizadeh
+ */
+public class DsmDaemon extends DsmManifest implements Daemon {
+
+    /**
+     * Daemon name.
+     */
+    private static final String DAEMON_NAME = "Dsm Daemon";
+
+    /**
+     * Daemon initialization state.
+     */
+    private transient boolean inited = false;
+
+    /**
+     * DsmDaemon Constructor.
+     */
+    public DsmDaemon() {
+        super("efae8bdb-2647-45e6-803e-c7ff2976a3e6", "DSM Daemon");
+    }
+
+    /**
+     * Returns daemon name.
+     *
+     * @return Daemon name
+     */
+    public final String getDaemonName() {
+        return DsmDaemon.DAEMON_NAME;
+    }
+
+    /**
+     * Returns whether daemon is initialized.
+     *
+     * @return Daemon initialization status
+     */
+    public final boolean isInited() {
+        return this.inited;
+    }
+
+    /**
+     * Initializes daemon.
+     *
+     * @param initializationParams Contains initializations parameters
+     */
+    public final void init(final String initializationParams) {
+        this.inited = true;
+    }
+
+    /**
+     * Shutdown daemon.
+     */
+    public final void shutdown() {
+        this.inited = false;
+    }
+}
