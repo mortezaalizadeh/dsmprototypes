a89cfd39ff81abf703e1a48639d1a996ff653441
morteza.alizadeh@gmail.com <morteza.alizadeh@gmail.com@40325e92-97ac-80f2-9294-f2279de5e1f8>
2012-11-05 08:54:52 +0000

    updated.
    git-svn-id: https://dsmprototypes.googlecode.com/svn/trunk@153 40325e92-97ac-80f2-9294-f2279de5e1f8

---
+++ src/managers/action_managers/SimpleActionManager/src/main/java/dsm/actionmanager/SimpleActionManager.java
@@ -0,0 +1,67 @@
+/**
+ * @author Morteza Alizadeh
+ */
+package dsm.actionmanager;
+
+import dsm.action.manager.common.ActionManager;
+import dsm.common.DsmManifest;
+
+/**
+ * SimpleActionManager is a simple action manager.
+ *
+ * @version 0.1, May 21, 2012
+ * @author Morteza Alizadeh
+ */
+public class SimpleActionManager extends DsmManifest implements ActionManager {
+
+    /**
+     * Action manager name.
+     */
+    private static final String ACTION_MANAGER_NAME = "Simple Action Manager";
+
+    /**
+     * Action manager initialization state.
+     */
+    private transient boolean inited = false;
+
+    /**
+     * SimpleActionManager Constructor.
+     */
+    public SimpleActionManager() {
+        super("114cae51-e05b-4961-99f4-06df89eb022b", "Simple Action Manager");
+    }
+
+    /**
+     * Returns action manager name.
+     *
+     * @return Action manager name
+     */
+    public final String getActionManagerName() {
+        return SimpleActionManager.ACTION_MANAGER_NAME;
+    }
+
+    /**
+     * Returns whether action manager is initialized.
+     *
+     * @return Action manager initialization status
+     */
+    public final boolean isInited() {
+        return this.inited;
+    }
+
+    /**
+     * Initializes action manager.
+     *
+     * @param initializationParams Contains initializations parameters
+     */
+    public final void init(final String initializationParams) {
+        this.inited = true;
+    }
+
+    /**
+     * Shutdown action manager.
+     */
+    public final void shutdown() {
+        this.inited = false;
+    }
+}
